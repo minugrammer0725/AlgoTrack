7201c8a0c3c2a579f15c08ab0bd682f315ab54b6
minugrammer0725 <kwawkminoo@gmail.com>
2023-02-08 10:38:27 -0800

    Layout for main content and sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { FC, ReactElement } from "react";
 import { ThemeProvider, CssBaseline} from "@mui/material";
 import { customTheme } from './styles/Theme';
 
+import Dashboard from './pages/dashboard/Dashboard'
+
 const App: FC = (): ReactElement => {
 
   return (
     <ThemeProvider theme={customTheme}>
       <CssBaseline />
-      <h1>Hello App</h1>
+      <Dashboard />
     </ThemeProvider>
   )
 }
