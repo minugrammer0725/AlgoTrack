f62d0310d47437e9785aac4755fe218cf7519afa
minugrammer0725 <kwawkminoo@gmail.com>
2023-02-08 10:49:01 -0800

    created user profile component

---
+++ src/components/profile/Profile.tsx
@@ -0,0 +1,17 @@
+import { FC, ReactElement } from 'react';
+
+import { Avatar, Box, Typography } from '@mui/material';
+
+const Profile: FC = (): ReactElement => {
+  return (
+    // Avatar
+    
+    // Name
+
+    // Welcome Message
+
+    <h2>Profile..</h2>
+  )
+}
+
+export default Profile;
