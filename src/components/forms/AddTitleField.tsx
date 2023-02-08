ba193b39809cf27787e0888072a2ce696cbc24aa
minugrammer0725 <kwawkminoo@gmail.com>
2023-02-08 15:10:09 -0800

    title field

---
+++ src/components/forms/AddTitleField.tsx
@@ -0,0 +1,23 @@
+import { FC, ReactElement } from 'react';
+import { TextField } from '@mui/material';
+import { ITextField } from '../interfaces/ITextField';
+import PropTypes from 'prop-types';
+
+const AddTitleField: FC<ITextField> = ({
+    onChange = (e) => console.log(e), 
+    disabled = false
+  }): ReactElement => {
+  return (
+    <TextField id='title' name='title' label='Title' placeholder='Problem Title'
+      variant='outlined' size='small' fullWidth 
+      onChange={onChange}
+      disabled={disabled} />
+  )
+}
+
+AddTitleField.propTypes = {
+  onChange: PropTypes.func,
+  disabled: PropTypes.bool
+}
+
+export default AddTitleField;
