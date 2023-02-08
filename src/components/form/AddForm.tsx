482e38c9de09eb4b3682e2e1eb9ca702d9dbf7d7
minugrammer0725 <kwawkminoo@gmail.com>
2023-02-08 14:09:45 -0800

    due date form

--- src/components/form/AddForm.tsx
+++ src/components/form/AddForm.tsx
@@ -1,8 +1,15 @@
-import { FC, ReactElement} from 'react';
+import { useState, FC, ReactElement} from 'react';
 
 import { Box, Typography, Stack, TextField } from '@mui/material';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+
 
 const AddForm: FC = (): ReactElement => {
+  // const [title, setTitle] = useState<string>('');
+  // const [code, setCode] = useState<string>('');
+  const [date, setDate] = useState<Date|null>(null);
   return (
     <Box display='flex' flexDirection={'column'} alignItems='flex-start'
       width={'100%'} px={4} my={6}>
@@ -14,11 +21,17 @@ const AddForm: FC = (): ReactElement => {
           variant='outlined' size='small' fullWidth 
           onChange={(e) => console.log(e.target.value)}/>
         {/* Category  */}
+        
         {/* Code  */}
         <TextField id='code' name='code' label='code' placeholder='Your Code Here' 
           variant='outlined' size='small' multiline rows={8} fullWidth 
           onChange={(e) => console.log(e.target.value)}/>
         {/* Due Date  */}
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <DesktopDatePicker renderInput={(params) => (<TextField {...params}/>)}
+            value={date} label='Due Date' inputFormat='mm/dd/yyyy'
+            onChange={(newDate) => setDate(newDate)} />
+        </LocalizationProvider>
         {/* Status  */}
         {/* Priority  */}
       </Stack>
