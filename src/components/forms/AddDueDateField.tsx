fa63e9891ff0b8057ede8132a06f4c4b8dd872b8
minugrammer0725 <kwawkminoo@gmail.com>
2023-02-08 15:10:06 -0800

    due date field

---
+++ src/components/forms/AddDueDateField.tsx
@@ -0,0 +1,30 @@
+import { FC, ReactElement } from 'react';
+import { TextField } from '@mui/material';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+import { IDateField } from '../interfaces/IDateField';
+import PropTypes from 'prop-types';
+
+const AddDueDateField: FC<IDateField> = ({
+  disabled = false,
+  value = new Date(),
+  onChange = (date)=>console.log(date)
+}): ReactElement => {
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <DesktopDatePicker renderInput={(params) => (<TextField {...params}/>)}
+        value={value} label='Due Date' inputFormat='mm/dd/yyyy'
+        onChange={onChange} disabled={disabled} />
+    </LocalizationProvider>  )
+}
+
+AddDueDateField.propTypes = {
+  disabled: PropTypes.bool,
+  value: PropTypes.instanceOf(Date),  
+  onChange: PropTypes.func
+}
+
+
+export default AddDueDateField;
