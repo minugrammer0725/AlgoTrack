dd085d99055aa199319e32726350474fda3111fb
minugrammer0725 <kwawkminoo@gmail.com>
2023-02-08 10:59:00 -0800

    user avatar using MUI Avatar, Box, Typography

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -4,13 +4,23 @@ import { Avatar, Box, Typography } from '@mui/material';
 
 const Profile: FC = (): ReactElement => {
   return (
-    // Avatar
-    
-    // Name
+    <Box display={'flex'} flexDirection={'column'}
+      justifyContent={'center'} alignItems={'center'}>
+      {/* Avatar */}
+      <Avatar sx={{
+        width: '96px',
+        height: '96px',
+        backgroundColor: 'primary.main',
+        marginBottom: '16px'
+      }}>
+        <Typography variant='h4' color={'text.primary'}>MW</Typography>
+      </Avatar>
+      {/* Name */}
+      <Typography variant='h6' color={'text.primary'}>Welcome, Minwoo</Typography>
+      {/* Message */}
+      <Typography variant='body1' color={'text.primary'}>This is your personal Leetcode Tracker!</Typography>
+    </Box>
 
-    // Welcome Message
-
-    <h2>Profile..</h2>
   )
 }
 
