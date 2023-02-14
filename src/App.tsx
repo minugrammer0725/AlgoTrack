5a8ee37e7145c7c63f70dff870b7bd4a694d86fa
minugrammer0725 <kwawkminoo@gmail.com>
2023-02-14 15:17:24 -0800

    integrated react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import { FC, ReactElement } from "react";
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider, CssBaseline} from "@mui/material";
 import { customTheme } from './styles/Theme';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import Dashboard from './pages/dashboard/Dashboard'
+
+// query client
+const queryClient = new QueryClient();
 
 const App: FC = (): ReactElement => {
 
   return (
-    <ThemeProvider theme={customTheme}>
-      <CssBaseline />
-      <Dashboard />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={customTheme}>
+        <CssBaseline />
+        <Dashboard />
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false}/>
+    </QueryClientProvider>
+    
   )
 }
 
