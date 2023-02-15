8cdea5ceb32f8ee3579aaa81a6f070e9873d4453
minugrammer0725 <kwawkminoo@gmail.com>
2023-02-15 15:01:02 -0800

    added conditional alert and load cards from DB

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,14 +1,24 @@
 import { FC, ReactElement } from 'react';
-
-import { Grid, Box } from '@mui/material';
+import { Grid, Box, Alert, LinearProgress } from '@mui/material';
 import { format } from 'date-fns';
-
 import { Status } from '../enums/Status';
+import { useQuery } from '@tanstack/react-query';
+import { sendApiRequest } from '../helpers/sendApiRequest';
+import { ICardApi } from '../interfaces/ICardApi';
 
 import FlashCard from './flashcard/FlashCard';
 import Counter from './counter/Counter';
 
 const Content: FC = (): ReactElement => {
+  const {error, isLoading, data, refetch} = useQuery(
+    ['cards'],
+    async () => {
+      return await sendApiRequest<ICardApi[]>(
+        'http://localhost:3000/cards',
+        'GET'
+      );
+    },
+  );
 
   return (
     <Grid item md={8} px={4}>
@@ -26,10 +36,27 @@ const Content: FC = (): ReactElement => {
         </Grid>
         <Grid item display='flex' flexDirection='column'
           xs={10} md={8} >
-          {/* Problems */}
-          <FlashCard />
-          <FlashCard />
-          <FlashCard />
+          <>
+            {/* Alert  */}
+            {error && 
+              <Alert severity='error'>
+                There was an error fetching your cards
+              </Alert>}
+            {!error && !data?.length && 
+              <Alert severity='warning'>
+                You do not have any cards yet
+              </Alert>}
+            {isLoading ? <LinearProgress /> : (
+              Array.isArray(data) && data.length>0 && 
+              data.map((card) => (
+                <FlashCard id={card.id} key={card.id}
+                  title={card.title} date={new Date(card.date)}
+                  body={card.code} difficulty={card.difficulty}
+                  status={card.status} />
+              ))
+            )}
+            {/* Problems */}
+          </>
         </Grid>
       </Grid>
     
