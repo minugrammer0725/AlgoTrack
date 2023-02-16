7f54c1fac242ff48e552a05065930aa1430c7643
minugrammer0725 <kwawkminoo@gmail.com>
2023-02-16 09:50:33 -0800

    added id param to both event handler functions

--- src/components/interfaces/ICardFooter.ts
+++ src/components/interfaces/ICardFooter.ts
@@ -3,8 +3,11 @@ import { ChangeEvent, MouseEvent } from 'react';
 export interface ICardFooter {
   id: string;
   status?: string;
-  onStatusChange?: (e: ChangeEvent<HTMLInputElement>) => void;
-  onClick?: (e: 
-    MouseEvent<HTMLButtonElement> | 
-    MouseEvent<HTMLAnchorElement>) => void;
+  onStatusChange?: (
+    e: ChangeEvent<HTMLInputElement>,
+    id: string) => void;
+  onClick?: (
+    e: MouseEvent<HTMLButtonElement> | 
+    MouseEvent<HTMLAnchorElement>,
+    id: string) => void;
 };
